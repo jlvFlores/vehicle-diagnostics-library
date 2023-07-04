e66a5cefc4f276fc0c32e4a32edd0c646c30205c
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-07-03 19:08:47 -0600

    Remove vite template

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'; // Import Provider from react-redux
 import store from './app/store'; // Import your Redux store
-import App from './App.jsx'
-import './index.css'
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
