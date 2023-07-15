1da19398cc785bfc22d3b95245acff7270f9952a
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-07-15 16:53:37 -0600

    Update Header's return navlink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <h1 className="logo">E.M.A.T.</h1>
-    <nav>
-      <div>
-        <NavLink className="header-link" to="/">Return</NavLink>
-      </div>
-      <div>
-        <NavLink className="header-link" to="/login">Login</NavLink>
-        <NavLink className="header-link" to="/register">Register</NavLink>
-      </div>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const { id } = useParams();
+
+  return (
+    <header>
+      <h1 className="logo">E.M.A.T.</h1>
+      <nav>
+        <div>
+          <NavLink id="return-link" className={`header-link ${id !== undefined ? 'return-link' : ''}`} to="/">Return</NavLink>
+        </div>
+        <div>
+          <NavLink className="header-link" to="/login">Login</NavLink>
+          <NavLink className="header-link" to="/register">Register</NavLink>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
