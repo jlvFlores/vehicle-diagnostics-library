7880d694256d4182d323c1e6e9c4ba12cb2b7415
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-09-22 12:04:00 -0600

    Remove excess React imports

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchValue } from '../app/searchbar/searchbarSlice';
 import SearchItem from './SearchItem';
