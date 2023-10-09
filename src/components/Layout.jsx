e360395888d34a8e5b9fad99ceec150e9369db84
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-10-09 16:36:06 -0600

    Add Footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout = () => (
   <main>
     <Header />
     <Outlet />
+    <Footer />
   </main>
 );
 
